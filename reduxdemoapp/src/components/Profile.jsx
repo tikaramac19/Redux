311a84a00e398942dde331f87a02be91e74081a7
tikaramac19 <tikaramac19@gmail.com>
2022-07-05 22:51:17 +0545

    profile site

---
+++ reduxdemoapp/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Profile.module.css";
+const Profile = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Welcome To The User Profile Page</h2>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
