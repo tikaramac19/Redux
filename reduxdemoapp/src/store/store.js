b7b4b05be40fe4fe90e5b0cbc9d247dc3205fb8c
tikaramac19 <tikaramac19@gmail.com>
2022-07-06 21:05:16 +0545

    counter reducer added

--- reduxdemoapp/src/store/store.js
+++ reduxdemoapp/src/store/store.js
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './Slice/StateSlice'
+import counterReducer from './Slice/CounterSlice'
+
 export default configureStore({
     reducer : {
-        auth: authReducer
+        auth: authReducer,
+        counter : counterReducer
     }
 })
