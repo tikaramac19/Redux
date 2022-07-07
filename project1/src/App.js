0193a69d684d4af46400e08ba79612c06fb7ad8b
tikaramac19 <tikaramac19@gmail.com>
2022-07-07 20:48:04 +0545

    adding userslist

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import AddUsers from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
 function App() {
   return (
     <>
       <AddUsers />
+
+      <UsersList users = {[]}/>
     </>
   );
 }
