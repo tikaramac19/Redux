45ed08cc80cbe93bab98c569978a23c945dd36f3
tikaramac19 <tikaramac19@gmail.com>
2022-07-07 18:20:03 +0545

    unchange handle using usestate

--- project1/src/components/Users/AddUsers.jsx
+++ project1/src/components/Users/AddUsers.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Users/Users.module.css";
 import Card from "../Ui/Card";
 
 const AddUsers = (props) => {
-
-
-
+  const [user, setUser] = useState("");
+  const [age, setAge] = useState("");
 
   const AddUserHandler = (e) => {
-
     e.preventDefault();
+
+    console.log(user, age);
   };
 
+  const usernameHandle = (e) =>{
+    e.preventDefault();
+
+    setUser(e.target.value)
+
+  }
+
+  const ageHandle = (e) =>{
+    e.preventDefault();
+
+    setAge(e.target.value)
+    
+  }
+
   return (
-    
-      
-       <div className={styles.container}>
-         <form onSubmit={AddUserHandler} className= {styles.form}>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="username" />
+    <div className={styles.container}>
+      <form onSubmit={AddUserHandler} className={styles.form}>
+        <label htmlFor="username">Username</label>
+        <input type="text" value={user} id="username" placeholder="username" onChange={usernameHandle}/>
 
-          <label htmlFor="age">Age(Years)</label>
-          <input type="number" id="age" placeholder="age"/>
+        <label htmlFor="age">Age(Years)</label>
+        <input type="number" value={age} id="age" placeholder="age" onChange={ageHandle}/>
 
-          <button type="submit">Add User</button>
-        </form>
-      
-       </div>
-    
+        <button type="submit">Add User</button>
+      </form>
+    </div>
   );
 };
 
