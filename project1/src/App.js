c4e1f4a55ce34620e5c9e941ddc4bcbf0af031ff
tikaramac19 <tikaramac19@gmail.com>
2022-07-07 21:15:54 +0545

    act as middleware for two components

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,12 +1,22 @@
+import React, {useState} from "react";
 import "./App.css";
 import AddUsers from "./components/Users/AddUsers";
 import UsersList from "./components/Users/UsersList";
 function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) =>{
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name : uName, age : uAge, id: Math.random().toString()}]
+    });
+  }
+
   return (
     <>
-      <AddUsers />
+      <AddUsers onAddUser = {addUserHandler} />
 
-      <UsersList users = {[]}/>
+      <UsersList users = {usersList}/>
     </>
   );
 }
