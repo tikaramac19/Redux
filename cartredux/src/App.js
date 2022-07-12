ec83d2c09a95ddb03c987cf5fb62b74743f22ff6
tikaramac19 <tikaramac19@gmail.com>
2022-07-12 21:39:19 +0545

    navbar added

--- cartredux/src/App.js
+++ cartredux/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Layout/Navbar';
 function App() {
   return (
     <>
     
-    
+      <Navbar />
     
     </>
   );
