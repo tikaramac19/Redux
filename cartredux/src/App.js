1837d681b8e81a1a3dbef08ebcc1395072567429
tikaramac19 <tikaramac19@gmail.com>
2022-07-13 19:40:43 +0545

    added cart comp

--- cartredux/src/App.js
+++ cartredux/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Layout/Navbar';
+import ShopCart from './components/Shop/ShopCart';
 function App() {
   return (
     <>
     
       <Navbar />
-    
+      <ShopCart />
     </>
   );
 }
