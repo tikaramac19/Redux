8bd51c5a9f7c911dd726bad73f6f6e5fcf72fa68
tikaramac19 <tikaramac19@gmail.com>
2022-07-14 21:39:12 +0545

    crt btn

--- 01-starting-project/src/components/Cart/CartButton.js
+++ 01-starting-project/src/components/Cart/CartButton.js
@@ -1,21 +1,18 @@
-import classes from './CartButton.module.css';
-import { toggle } from '../../store/ui-slice';
-import {useSelector, useDispatch} from 'react-redux'
+import classes from "./CartButton.module.css";
+import { toggle } from "../../store/ui-slice";
+import { useSelector, useDispatch } from "react-redux";
 
 const CartButton = (props) => {
-
   // const toggle = useSelector((state)=> state.ui.toggle)
 
   const dispatch = useDispatch();
 
-  const toggleCartHandler = () =>{
-
+  const toggleCartHandler = () => {
     dispatch(toggle());
-
-  }
+  };
 
   return (
-    <button className={classes.button} onClick = {toggleCartHandler }>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
