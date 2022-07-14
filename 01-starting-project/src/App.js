b6da002a0be2ad896a73bf8fb98e1d3aa8a62d09
tikaramac19 <tikaramac19@gmail.com>
2022-07-14 21:26:33 +0545

    conditionally rendering cart comp

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,19 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import {useSelector} from 'react-redux'
+
+function App() {
+
+  const toggle = useSelector((state) => state.ui.cartIsVisible);
+
+  console.log(toggle)
+  return (
+    <Layout>
+      {toggle && <Cart />}
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
